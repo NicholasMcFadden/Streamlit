e9c9657b979ad1703be420a948ca645106962f3d
NicholasMcFadden <97934765+NicholasMcFadden@users.noreply.github.com>
2023-05-21 17:07:51 -0700

    name of files change & additional page added tools

--- First_App/app.py
+++ First_App/app.py
@@ -1,17 +1,41 @@
 import streamlit as st
 import requests
 
+import pandas as pd
+import numpy as np
+
+
 def main():
-    menu = ['Home',"About"]
+    menu = ['Home',"About", "Tools"]
     Nav_Sel = st.sidebar.selectbox('Menu', menu)
 
-    st.title("AllJobs - Job Agreggator")
+    st.title("First Streamlit App")
 
-    if Nav_Sel == "Home":
-        st.subheader('Home')
+    if Nav_Sel == "About":
+        st.subheader('About')
+   
+
+
+    elif Nav_Sel == "Tools":
+        st.subheader('Tools')
+
+        col1,col2 = st.columns(1,3)
+
+          
+        with col1:
+            st.subheader('Convert')
+            st.title('Currency')
+            st.selectbox('Pick ', ['', 'dogs'])
+
+
+
+
+
+
+
 
     else:
-        st.subheader("About")
+        st.subheader("Home")
 
 if __name__ == '__main__':
     main()

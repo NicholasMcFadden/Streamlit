a5c282742d9e4b3e40fc3f8d93e4f69960582f38
NicholasMcFadden <97934765+NicholasMcFadden@users.noreply.github.com>
2023-05-21 21:40:38 -0700

    added columns to Tools; connected to currency api

--- First_App/app.py
+++ First_App/app.py
@@ -4,6 +4,8 @@ import requests
 import pandas as pd
 import numpy as np
 
+key = "6b49ba5510ccf54441a7af0498a76b563cc3a8d8"
+
 
 def main():
     menu = ['Home',"About", "Tools"]
@@ -19,22 +21,35 @@ def main():
     elif Nav_Sel == "Tools":
         st.subheader('Tools')
 
-        col1,col2 = st.columns(1,3)
+        col1,col2 = st.columns([1,3])
 
           
         with col1:
             st.subheader('Convert')
             st.title('Currency')
-            st.selectbox('Pick ', ['', 'dogs'])
+            st.selectbox('Pick ', ['AS', 'dogs'])
 
         with col2:
             st.subheader('Currency History')
 
-st.selectbox('Pick one', ['cats', 'dogs'])
+            # st.dataframe()
 
+            parameters = {"api_key": key, "format": "json",'from':'EUR','to':'GBP','amount':10}
 
+            url = "https://api.getgeoapi.com/v2/currency/convert"
 
+            response = requests.get(url, parameters)
 
+            st.write(response.json())
+            
+            
+
+            # GET: https://api.getgeoapi.com/v2/currency/convert
+            # ?api_key=6b49ba5510ccf54441a7af0498a76b563cc3a8d8
+            # &from=EUR
+            # &to=GBP
+            # &amount=10
+           
 
 
 

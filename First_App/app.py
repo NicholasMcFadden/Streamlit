20867679f2c221d183611514253ea3a744a2234e
NicholasMcFadden <97934765+NicholasMcFadden@users.noreply.github.com>
2023-05-22 02:03:59 -0700

    all avail cur list api brought in for drop select

--- First_App/app.py
+++ First_App/app.py
@@ -38,19 +38,44 @@ def main():
 
                 cur_list = ['USD', 'EUR','GBP']
 
+                parameters = {"api_key":st.secrets["db_password"], "format": "json"}
 
-                start_cur = st.selectbox('From: ', cur_list)
+                url = "https://api.getgeoapi.com/v2/currency/list"
+
+                response = requests.get(url, parameters)
+
+                data_list = response.json()
+
+                cur_list = data_list['currencies']
+
+                start_cur = st.selectbox('From: ',cur_list.values())
+
+                start_pos = [key for key, val in cur_list.items() if val == start_cur]
+                
+                if len(start_pos) > 0:
+                    print("The key for the value", start_cur, "is", start_pos[0])
+                else:
+                    print("Value not found in dictionary")
 
                 amount_base_cur = st.number_input('Amount: ',min_value=0)
 
-                end_cur = st.selectbox('To: ', cur_list[::-1])
+                end_cur = st.selectbox('To: ', cur_list.values())
+
+                end_pos = [key for key, val in cur_list.items() if val == end_cur]
+                
+                if len(end_pos) > 0:
+                    print("The key for the value", end_cur, "is", end_pos[0])
+                else:
+                    print("Value not found in dictionary")
+
+                              
     
             with col2:
 
                 st.subheader('Currency Exchange Info')
 
                 parameters = {"api_key":st.secrets["db_password"], "format": "json",
-                            "from":start_cur,"to":end_cur,'amount':amount_base_cur}
+                            "from":start_pos[0],"to":end_pos[0],'amount':amount_base_cur}
 
                 url = "https://api.getgeoapi.com/v2/currency/convert"
 
@@ -67,11 +92,10 @@ def main():
                 conv_df_t = conv_df.transpose()
 
                 st.write(conv_df_t)
-
                 
                 st.write('API JSON Response: ',data_ext)
 
-                
+                           
     else:
         st.subheader("Home")
 

f7279acf43cce5c7da68c59994d1615778646ccb
NicholasMcFadden <97934765+NicholasMcFadden@users.noreply.github.com>
2023-05-22 03:50:45 -0700

    new branch for multipage solution

---
+++ First_App/main_page.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import requests
+
+import pandas as pd
+import numpy as np
+
+import helpers
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+    st.title("First Streamlit App")
---
+++ First_App/pages/about.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import requests
+
+import pandas as pd
+import numpy as np
+
+import helpers
+
+
+st.markdown("# About ❄️")
+st.sidebar.markdown("# About ❄️")
